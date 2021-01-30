96467765ec65fb7b706b09bfddfb38fdfa685e26
Moyewa Odiase <moyeodiase@hotmail.co.uk>
2021-01-30 22:44:22 +0000

    Finished 23.

---
+++ solutions/23/solution.py
@@ -0,0 +1,73 @@
+
+
+import logging 
+from typing import cast, Generator, List, Optional, Tuple
+
+logging.basicConfig(format="%(asctime)s - %(lineno)d - %(funcName)s: %(message)s", level=logging.DEBUG)
+
+TTile = Tuple[int,int]
+TMatrix = List[List[bool]]
+
+def tile_is_not_wall(tile: TTile, matrix : TMatrix) -> bool:
+    return not matrix[tile[0]][tile[1]]
+    
+def tile_is_within_matrix_limits(tile : TTile, matrix : TMatrix) -> bool:
+    if tile[0] < 0 or tile[1] < 0:
+        return False
+    return tile[0] < len(matrix) and tile[1] < len(matrix[tile[0]])
+
+def generate_moves(start : TTile) -> Generator[TTile,None,None]:
+    x,y = start
+    yield (x-1,y)
+    yield (x+1,y)
+    yield (x,y-1)
+    yield (x,y+1)
+
+def valid_moves(start: TTile, matrix : TMatrix) -> List[TTile]:
+    return [ move for move in generate_moves(start=start)
+            if tile_is_within_matrix_limits(tile=move,matrix=matrix)
+            and tile_is_not_wall(tile=move,matrix=matrix)]
+
+def compute_all_paths(start: TTile, end: TTile,
+                      matrix: TMatrix) -> Optional[List[List[TTile]]]:
+    def _compute_all_paths(start: TTile, end: TTile,
+                      matrix: TMatrix, history: List[TTile]) -> Optional[List[List[TTile]]]:
+        if start == end:
+            return [[end]]
+
+        next_starts = [ next_start for next_start in valid_moves(start, matrix=matrix)
+                       if next_start not in history ]
+
+        if next_starts == []:
+            return None
+
+        paths : List[List[TTile]] = []
+        for next_start in next_starts: 
+            computed_paths = _compute_all_paths(start=next_start, 
+                                     end=end, matrix=matrix, 
+                                     history=[*history,next_start])
+            if not computed_paths:
+                continue
+            for path in computed_paths:
+                paths.append(path) 
+        
+        if not paths:
+            return None 
+        
+
+        return [ [start,*path] for path in paths]
+
+    return _compute_all_paths(start=start,end=end,
+                              matrix=matrix,history=[start])
+       
+
+def main(start : TTile, end : TTile, 
+         matrix: TMatrix ) -> Optional[int]:
+    
+    paths = compute_all_paths(start=start, end=end,matrix=matrix)
+    if paths == []:
+        return None
+    paths = cast(List[List[TTile]],paths)
+    length_of_shortest_path = min([ len(path) for path in paths])
+    return length_of_shortest_path - 1
+
---
+++ solutions/23/tests.py
@@ -0,0 +1,69 @@
+import unittest
+
+from .solution import compute_all_paths, main, valid_moves, \
+    generate_moves, tile_is_within_matrix_limits, tile_is_not_wall 
+
+class TestCase(unittest.TestCase):
+    def setUp(self):
+        self.basic_matrix = [[False,False],[False,False]]
+        self.given_matrix = [[False, False, False, False],
+                                   [True, True, False, True],
+                                   [False, False, False, False],
+                                   [False, False, False, False]]
+        
+    
+    def test_tile_is_not_wall(self):
+        self.assertTrue(tile_is_not_wall(tile=(0,0), matrix=self.basic_matrix))
+        self.assertTrue(tile_is_not_wall(tile=(1,2), matrix=self.given_matrix))
+        
+    def test_tile_is_wall(self):
+        self.assertFalse(tile_is_not_wall(tile=(1,0), matrix=self.given_matrix))
+        self.assertFalse(tile_is_not_wall(tile=(1,3), matrix=self.given_matrix))
+        
+    def test_tile_is_within_matrix_limits(self):
+        self.assertTrue(tile_is_within_matrix_limits(tile=(0,0),matrix=self.basic_matrix))
+        self.assertTrue(tile_is_within_matrix_limits(tile=(0,1),matrix=self.basic_matrix))
+        self.assertTrue(tile_is_within_matrix_limits(tile=(1,0),matrix=self.basic_matrix))
+        self.assertTrue(tile_is_within_matrix_limits(tile=(1,1),matrix=self.basic_matrix))
+
+    def test_tile_is_not_within_matrix_limits(self):
+        self.assertFalse(tile_is_within_matrix_limits(tile=(-1,0),matrix=self.basic_matrix))
+        self.assertFalse(tile_is_within_matrix_limits(tile=(0,10),matrix=self.basic_matrix))
+        self.assertFalse(tile_is_within_matrix_limits(tile=(100,100),matrix=self.basic_matrix))
+        self.assertFalse(tile_is_within_matrix_limits(tile=(-1,-10),matrix=self.basic_matrix))
+
+    def test_given(self):
+        given_input = { 'start': (3,0), 
+                       'end' : (0,0), 
+                       'matrix' : self.given_matrix }
+        expected_result = 7
+        computed_result = main(**given_input)
+        self.assertEqual(expected_result,computed_result)
+
+    def test_valid_moves_with_basic_matrix(self):
+        expected_result = [(1,0),(0,1)]
+        computed_result = valid_moves((0,0),self.basic_matrix)
+        self.assertEqual(expected_result,computed_result)
+    
+    def test_valid_moves_with_given_matrix(self):
+        expected_result = [(0,2),(2,2)]
+        computed_result = valid_moves((1,2),self.given_matrix)
+        self.assertEqual(expected_result,computed_result)
+
+    def test_compute_all_paths(self):
+        expected_result = [[(0,0),(0,1),(1,1)],[(0,0),(1,0),(1,1)]]
+        computed_result = compute_all_paths(start=(0,0), 
+                                            end=(1,1),
+                                            matrix=self.basic_matrix)
+        assert computed_result is not None
+        self.assertEqual(sorted(expected_result),sorted(computed_result))
+    
+    def test_generate_moves(self):
+        expected_result = [(-1,0),(1,0),(0,-1),(0,1)]
+        computed_result = list(generate_moves((0,0)))
+        self.assertEqual(expected_result,computed_result)
+        
+        
+
+if __name__ == "__main__":
+    unittest.main()
