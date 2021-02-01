9a5c4afe7fc7717eeea4c8a9d5267926b7015ac6
Moyewa Odiase <moyeodiase@hotmail.co.uk>
2021-02-01 21:54:28 +0000

    Finished 24.

---
+++ solutions/24/solution.py
@@ -0,0 +1,115 @@
+from __future__ import annotations
+from abc import ABC, abstractmethod 
+from collections import deque
+from typing import Any, Generic, Iterator, Optional, Type, TypeVar
+
+
+T = TypeVar('T')
+
+class TraversalStrategy(ABC):
+    def __init__(self,node : Node[Any]):
+        self.node = node
+    
+    
+    def __iter__(self)-> Iterator[Node[Any]]:
+        return self
+
+    @abstractmethod
+    def __next__(self) -> Node[Any]:
+        ...
+    
+    @abstractmethod
+    def update(self):
+        ...
+
+
+class BreadthFirstSearch(TraversalStrategy):
+    def __init__(self, node : Node[Any]):
+        ...
+    
+
+class DepthFirstSearch(TraversalStrategy):
+    def __init__(self, node : Node[Any]):
+        super().__init__(node)
+        self._nodes = deque()
+        if left := self.node.left:
+            self._nodes.append(left)
+        if right := self.node.right:
+            self._nodes.append(right)
+   
+    def update(self):
+        self._nodes.clear()
+        if left := self.node.left:
+            self._nodes.append(left)
+        if right := self.node.right:
+            self._nodes.append(right)
+    
+    def __next__(self) -> Node[Any]:
+        if not self._nodes:
+            raise StopIteration
+        head = self._nodes.popleft()
+        if right := head.right:
+            self._nodes.appendleft(right)
+        if left := head.left:
+            self._nodes.appendleft(left)
+        return head
+        
+
+
+class Node(Generic[T]):
+    def __init__(self, payload : T, left : Optional[Node[Any]] = None, right : Optional[Node[Any]] = None,
+                 traversal_strategy : Type[TraversalStrategy] = DepthFirstSearch):
+        self.payload = payload
+        self._left = left
+        self._right = right
+        self.traversal_strategy = traversal_strategy(self)
+        self._is_locked = False
+    
+    @property
+    def left(self) -> Optional[Node[Any]]:
+        return self._left
+    
+    @left.setter
+    def left(self, left : Any):
+        self._left = left
+        self.traversal_strategy.update() # Could use subscriber pattern
+
+    @property
+    def right(self) -> Optional[Node[Any]]:
+        return self._right
+    
+    @right.setter
+    def right(self, right : Any):
+        self._right = right
+        self.traversal_strategy.update() # Could use subscriber pattern
+    
+    @property
+    def is_locked(self) -> bool:
+        return self._is_locked
+        
+    def lock(self) -> bool:
+        if self._is_locked or any([ n.is_locked for n in self]):
+            return False
+        self._is_locked = True
+        return True
+    
+    def unlock(self) -> bool:
+        if not self._is_locked or any([ n.is_locked for n in self]):
+            return False
+        self._is_locked = False
+        return True
+        
+        
+    
+    def __iter__(self) -> Iterator[Node[Any]]:
+        return self.traversal_strategy
+     
+    def __str__(self):
+        return f'<Node ({self.payload})>'
+    
+    def __repr__(self):
+        return f'Node(payload={self.payload},left={self.left},right={self.right},\
+            traversal_strategy={repr(self.traversal_strategy)})'
+        
+
+
---
+++ solutions/24/tests.py
@@ -0,0 +1,91 @@
+import unittest
+
+from .solution import Node
+
+
+
+class Tests(unittest.TestCase):
+    def setUp(self):
+        root = Node(5)
+        root.left = Node(10)
+        root.left.left = Node(15)
+        root.left.right = Node(20)
+        self.root = root
+        
+    def test_dfs(self):
+        computed_result = [ n.payload  for n in self.root]
+        expected_result = [10,15,20]
+        self.assertEqual(computed_result,expected_result)
+    
+    def test_locking_suceeds_when_children_are_unlocked(self):
+        root = Node(5)
+        root.left = Node(10)
+        root.right = Node(20)
+
+        computed_result = root.lock()
+
+        self.assertTrue(computed_result)
+        
+    
+    def test_locking_fails_when_one_child_is_locked(self):
+        root = Node(5)
+        root.left = Node(10)
+        root.right = Node(20)
+        root.left.left = Node(30)
+
+        root.left.left.lock()
+        computed_result = root.lock()
+
+        self.assertFalse(computed_result)
+    
+    def test_locking_fails_when_many_children_are_locked(self):
+        root = Node(5)
+        root.left = Node(10)
+        root.right = Node(20)
+        root.left.left = Node(30)
+
+        root.left.left.lock()
+        root.right.lock()
+        computed_result = root.lock()
+
+        self.assertFalse(computed_result)
+
+    def test_unlocking_suceeds_when_children_are_unlocked(self):
+        root = Node(5)
+        root.left = Node(10)
+        root.right = Node(20)
+
+        root.lock()
+        computed_result = root.unlock()
+
+        self.assertTrue(computed_result)
+    
+    def test_unlocking_fails_when_one_child_is_unlocked(self):
+        root = Node(5)
+        root.left = Node(10)
+        root.right = Node(20)
+        root.left.left = Node(30)
+
+        root.left.left.lock()
+        computed_result = root.unlock()
+
+        self.assertFalse(computed_result)
+    
+    def test_unlocking_fails_when_many_children_are_unlocked(self):
+        root = Node(5)
+        root.left = Node(10)
+        root.right = Node(20)
+        root.left.left = Node(30)
+
+        root.left.left.lock()
+        root.right.lock()
+        computed_result = root.unlock()
+
+        self.assertFalse(computed_result)
+
+    
+
+
+
+if __name__ == '__main__':
+    unittest.main()
