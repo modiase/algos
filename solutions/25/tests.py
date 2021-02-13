38f5a7832bc52b5073d11b9aa0046faac6a87234
Moye <moyeodiase@hotmail.co.uk>
2021-02-13 22:50:57 +0000

    Format change to 25.

--- solutions/25/tests.py
+++ solutions/25/tests.py
@@ -6,38 +6,42 @@ from .solution import main, tokenize, Token
 class TestTokenize(unittest.TestCase):
     def test_basic(self):
         test_pattern = 'a*.d.*'
-        expected_result = [Token('a',True),
+        expected_result = [Token('a', True),
                            Token('.'),
                            Token('d'),
-                           Token('.',True)
-                          ]
+                           Token('.', True)
+                           ]
 
         computed_result = tokenize(test_pattern)
 
-        self.assertEqual(expected_result,computed_result)
+        self.assertEqual(expected_result, computed_result)
+
+
 class TestSolution(unittest.TestCase):
     def test_given_which_is_expected_to_match_matches(self):
-        computed_result = main('ra.','ray')
+        computed_result = main('ra.', 'ray')
         self.assertTrue(computed_result)
 
     def test_given_which_is_expected_to_not_match_does_not_match(self):
-        computed_result = main('ra.','raymond')
+        computed_result = main('ra.', 'raymond')
         self.assertFalse(computed_result)
 
     def test_other_given_which_is_expected_to_match_matches(self):
-        computed_result = main('.*at','chat')
+        computed_result = main('.*at', 'chat')
         self.assertTrue(computed_result)
-    
+
     def test_complex_which_is_expected_to_match_matches(self):
-        computed_result = main('.*jk.*wee*.*weq','jkhkh;llkjkljklkjljl;weeklsfdjdsfj;weq')
+        computed_result = main(
+            '.*jk.*wee*.*weq', 'jkhkh;llkjkljklkjljl;weeklsfdjdsfj;weq')
         self.assertTrue(computed_result)
 
     def test_complex_which_is_expected_not_to_match_does_not_match(self):
-        computed_result = main('.*jk*.wee.*weqr','jkhkh;llkjkljklkjljl;weeklsfdjdsfj;weqtjhuafjs')
+        computed_result = main(
+            '.*jk*.wee.*weqr', 'jkhkh;llkjkljklkjljl;weeklsfdjdsfj;weqtjhuafjs')
         self.assertFalse(computed_result)
 
     def test_starred_consumes_none_or_many(self):
-        computed_result = main('.*chat','chat')
+        computed_result = main('.*chat', 'chat')
         self.assertTrue(computed_result)
 
 
