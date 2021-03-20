e150729a164ba2cb9067cdf46813587b2f1a3fb3
Moyewa Odiase <moyeodiase@hotmail.co.uk>
2021-03-20 23:31:32 +0000

    Finished 26.

---
+++ solutions/26/structs/LinkedList.py
@@ -0,0 +1,38 @@
+from __future__ import annotations 
+
+from typing import Generic, Optional, TypeVar
+
+from .Node import Node
+
+S = TypeVar('S')
+
+class LinkedList(Generic[S]):
+    def __init__(self, *args : S):
+        if not args:
+            return 
+        self.head = Node(args[0])
+        self._ptr = self.head
+        for arg in args[1:]:
+            node = Node(arg)
+            self._ptr.next = node
+            self._ptr = node
+        self._ptr = self.head
+    
+    def _return_next_and_advance_ptr(self) -> Optional[Node[S]]:
+        if not self._ptr:
+            return None
+        nxt = self._ptr
+        self._ptr = self._ptr.next
+        return nxt
+
+    def __iter__(self):
+        return self
+    
+    def __next__(self) -> S:
+        nxt = self._return_next_and_advance_ptr()
+        if not nxt:
+            raise StopIteration
+        return nxt.payload
+
+
+
---
+++ solutions/26/structs/Node.py
@@ -0,0 +1,10 @@
+from __future__ import annotations 
+
+from typing import Generic, Optional, TypeVar
+
+T = TypeVar('T')
+
+class Node(Generic[T]):
+    def __init__(self, payload: T, nextNode: Optional[Node[T]] = None):
+        self.payload = payload
+        self.next = nextNode
