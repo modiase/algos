5e1734f10c92a66b72de4834590e3f7fa283f061
Moyewa Odiase <moyeodiase@hotmail.co.uk>
2021-04-10 15:11:01 +0100

    Formatted code, changed names to increase readability.

--- solutions/28/solution.py
+++ solutions/28/solution.py
@@ -16,13 +16,17 @@ def main(words: List[str], k: int) -> List[str]:
         word = words.pop(0)
         if len(current_line + word) > k - 1:
             current_line = current_line.rstrip()
+
             logging.debug(f"Appending line: {current_line}.")
             lines.append(justify(current_line, k))
+
             current_line = word + " "
         else:
             logging.debug(f'Current line is: "{current_line}".')
+
             logging.debug(f'Adding word: "{word}".')
             current_line += (word + ' ')
+
             logging.debug(f'Current line is now: "{current_line}".')
 
     current_line = current_line.rstrip()
@@ -36,24 +40,28 @@ def justify(line: str, k: int) -> str:
     input_line = line
     current_line_len = len(line)
     while current_line_len < k:
-        line = pad_spaces_from_leftmost_evenly(line, k)
+        line = pad_spaces_by_adding_evenly_from_leftmost(line, k)
         current_line_len = len(line)
+
     logging.debug(f'Returning justified line: "{input_line}" -> "{line}"')
     return line
 
 
-def pad_spaces_from_leftmost_evenly(line: str, k: int) -> str:
-    index_and_size_of_spaces = compute_space_indices_and_size(line)
+def pad_spaces_by_adding_evenly_from_leftmost(line: str, k: int) -> str:
+    start_index_and_size_of_spaces = compute_start_index_and_size_of_each_space(
+        line)
 
     number_of_padding_spaces_needed = k - len(line)
-    index_and_size_of_required_spaces = add_spaces_evenly(index_and_size_of_spaces,
-                                                          number_of_padding_spaces_needed)
+    index_and_size_of_required_spaces = \
+        compute_required_start_index_and_size_of_each_space(
+            start_index_and_size_of_spaces,
+            number_of_padding_spaces_needed)
 
     required_sizes = [x[1] for x in index_and_size_of_required_spaces]
-    sizes = [x[1] for x in index_and_size_of_spaces]
+    sizes = [x[1] for x in start_index_and_size_of_spaces]
 
     diffs = [x[0] - x[1] for x in zip(required_sizes, sizes)]
-    indices = [x[0] for x in index_and_size_of_spaces]
+    indices = [x[0] for x in start_index_and_size_of_spaces]
 
     logging.debug(f"Indices to add spaces: {indices}.")
     reversed_indices = reversed(indices)
@@ -65,50 +73,63 @@ def pad_spaces_from_leftmost_evenly(line: str, k: int) -> str:
     return ''.join(line_as_list)
 
 
-def compute_space_indices_and_size(line: str) -> List[Tuple[int, int]]:
-    spaces_by_size_and_start_index: List[Tuple[int, int]] = []
+def compute_start_index_and_size_of_each_space(
+        line: str) -> List[Tuple[int, int]]:
+    spaces_by_start_index_and_size: List[Tuple[int, int]] = []
     offset = 0
     try:
         while idx := line.index(' ') + offset:
             trimmed = line[idx:]
+            size = len(trimmed) - len(trimmed.lstrip())
+
             logging.debug(f'Index is: "{idx}".')
             logging.debug(f'Trimmed is: "{trimmed}".')
             logging.debug(f'Trimmed lstripped is: "{trimmed.lstrip()}".')
-            size = len(trimmed) - len(trimmed.lstrip())
-            spaces_by_size_and_start_index.append((idx, size))
+
+            spaces_by_start_index_and_size.append((idx, size))
+
             offset += len(line) - len(trimmed.lstrip())
             line = trimmed.lstrip()
+
     except ValueError:
         pass
 
-    if len(spaces_by_size_and_start_index) == 0:
-        spaces_by_size_and_start_index.append((len(line), 0))
+    if len(spaces_by_start_index_and_size) == 0:
+        spaces_by_start_index_and_size.append((len(line), 0))
 
-    logging.debug(f'Returning spaces: {spaces_by_size_and_start_index}')
-    return spaces_by_size_and_start_index
+    logging.debug(f'Returning spaces: {spaces_by_start_index_and_size}.')
+    return spaces_by_start_index_and_size
 
 
-def add_spaces_evenly(index_and_size_of_spaces: List[Tuple[int, int]],
-                      number_of_padding_spaces_needed: int) -> List[Tuple[int, int]]:
-    index_and_size_of_spaces = index_and_size_of_spaces[:]
+def compute_required_start_index_and_size_of_each_space(
+        start_index_and_size_of_spaces: List[Tuple[int, int]],
+        number_of_padding_spaces_needed: int) -> List[Tuple[int, int]]:
+    required_start_index_and_size_of_spaces = start_index_and_size_of_spaces[:]
+
     while number_of_padding_spaces_needed > 0:
-        size_of_first_space = index_and_size_of_spaces[0][1]
-        if (all([x[1] == size_of_first_space
-                 for x in index_and_size_of_spaces])):
-            pair = index_and_size_of_spaces[0]
-            index_and_size_of_spaces[0] = (pair[0], pair[1] + 1)
+        size_of_first_space = required_start_index_and_size_of_spaces[0][1]
+
+        if (all([space_size == size_of_first_space
+                 for (_, space_size)
+                 in required_start_index_and_size_of_spaces])):
+
+            space_index, space_size = required_start_index_and_size_of_spaces[0]
+            required_start_index_and_size_of_spaces[0] = (
+                space_index, space_size + 1)
+            number_of_padding_spaces_needed -= 1
+
         else:
-            for i in range(1, len(index_and_size_of_spaces)):
-                if index_and_size_of_spaces[i][1] != size_of_first_space:
-                    pair = index_and_size_of_spaces[i]
-                    index_and_size_of_spaces[i] = (pair[0], pair[i] + 1)
+            for i in range(1, len(required_start_index_and_size_of_spaces)):
+                if number_of_padding_spaces_needed == 0:
                     break
-        number_of_padding_spaces_needed -= 1
 
-    return index_and_size_of_spaces
+                if required_start_index_and_size_of_spaces[i][1] == \
+                        size_of_first_space:
+                    continue
+                else:
+                    pair = required_start_index_and_size_of_spaces[i]
+                    required_start_index_and_size_of_spaces[i] = (
+                        pair[0], pair[i] + 1)
+                    number_of_padding_spaces_needed -= 1
 
-
-if __name__ == '__main__':
-    test_input = ["the", "quick", "brown", "fox",
-                  "jumps", "over", "the", "lazy", "dog"]
-    main(test_input, 16)
+    return required_start_index_and_size_of_spaces
