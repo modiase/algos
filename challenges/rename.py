4eb4d3fd82970283eb59ada16eab761b400729e3
Moye <moyeodiase@hotmail.co.uk>
2023-06-08 13:24:28 +0100

    Renamed files

--- challenges/rename.py
+++ challenges/rename.py
@@ -1,10 +1,14 @@
 import pathlib
+import sys
 import shutil
 
+dry_run = '-d' in sys.argv
 
 dir = pathlib.Path('./solutions')
 files = dir.glob("*")
 for path in files:
-    if len(path.name) < 4:
-        new_name = ('0'*4 + path.name)[-4:-1]
-        shutil.move(f'./solutions/{path.name}', f'./solutions/{new_name}')
+    if len(path.stem) < 4:
+        new_name = ('0'*4 + path.stem)[-4:] + path.suffix
+        print(f'{path.name} -> {new_name}')
+        if not dry_run:
+            shutil.move(f'./solutions/{path.name}', f'./solutions/{new_name}')
