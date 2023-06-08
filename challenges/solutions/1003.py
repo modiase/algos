38eb84213b9e877a484cb8c624451d4ebdc593e3
Moye <moyeodiase@hotmail.co.uk>
2023-06-08 10:18:43 +0100

    solved 1003

--- challenges/solutions/1003.py
+++ challenges/solutions/1003.py
@@ -17,3 +17,36 @@ The transitive closure of this graph would be:
 [0, 0, 0, 1]
 Given a graph, find its transitive closure.
 """
+
+graph = [
+    [0, 1, 3],
+    [1, 2],
+    [2],
+    [3]
+]
+
+
+def tclosure(g):
+    n = len(g)
+    mat = [[0 for _ in range(n)] for _ in range(n)]
+    for v in range(n):
+        for m in range(v, n):
+            if m in g[v]:
+                mat[v][m] = 1
+
+    for o in range(n):
+        for p in range(n):
+            connected = mat[o][p]
+            if connected:
+                for i in range(n):
+                    if mat[p][i] == 1:
+                        mat[o][i] = 1
+    return mat
+
+
+assert tclosure(graph) == [
+    [1, 1, 1, 1],
+    [0, 1, 1, 0],
+    [0, 0, 1, 0],
+    [0, 0, 0, 1],
+]
