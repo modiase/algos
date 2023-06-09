431b580ecc0c7223e9abede8a648deab7647db55
Moye <moyeodiase@hotmail.co.uk>
2023-06-09 08:48:16 +0100

    Solved 982

---
+++ solutions/0982.py
@@ -0,0 +1,26 @@
+"""
+A strobogrammatic number is a positive number that appears the same after being
+rotated 180 degrees. For example, 16891 is strobogrammatic. Create a program that
+finds all strobogrammatic numbers with N digits.
+
+Solved: ~5m
+"""
+import itertools
+
+
+def find_all(n):
+    digits = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+    all_permutations = itertools.product(digits, repeat=n)
+    filtered_permutations = [
+        p for p in all_permutations if ''.join(p).lstrip('0') == ''.join(p)]
+    found = []
+    for p in filtered_permutations:
+        s = ''.join(p)
+        if s == ''.join(reversed(s)):
+            found.append(s)
+    return found[:-1]
+
+
+print(find_all(4))
+print()
+print(find_all(3))
