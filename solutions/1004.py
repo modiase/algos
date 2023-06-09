91ccd1da01ac6eedede4afbe619170c9b10585e2
Moye <moyeodiase@hotmail.co.uk>
2023-06-09 19:33:03 +0100

    Addde 1004

---
+++ solutions/1004.py
@@ -0,0 +1,14 @@
+"""
+Given a list of numbers L, implement a method sum(i, j) which returns the sum from
+the sublist L[i:j] (including i, excluding j).
+
+For example, given L = [1, 2, 3, 4, 5], sum(1, 3) should return sum([2, 3]), which is 5.
+
+You can assume that you can do some pre-processing. sum() should be optimized over
+the pre-processing step.
+
+We will be sending the solution tomorrow, along with tomorrow's question. As alway
+, feel free to shoot us an email if there's anything we can help with.
+
+Have a great day!
+"""
