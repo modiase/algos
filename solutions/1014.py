c4ead7b6e30ed84f6d1ee877039e5d27c66de6a2
Moye <moyeodiase@hotmail.co.uk>
2023-06-19 21:41:54 +0100

    Solved 1014

--- solutions/1014.py
+++ solutions/1014.py
@@ -11,3 +11,49 @@ For example, given start = "dog", end = "cat", and dictionary = {"dot", "dop", "
 Given start = "dog", end = "cat", and dictionary = {"dot", "tod", "dat", "dar"}, return
 null as there is no possible transformation from dog to cat.
 """
+from typing import List
+d0 = [
+    "dot",
+    "dop",
+    "dat",
+    "cat",
+]
+
+d1 = [
+    "dot",
+    "tod",
+    "dat",
+    "dar"
+]
+
+
+def distance(w1: str, w2: str) -> int:
+    d = 0
+    for i in range(len(w1)):
+        d += int(w1[i] != w2[i])
+    return d
+
+
+def solve(start: str, end: str, dictionary: List[str]):
+
+    def dfs(current: str, path: List[str]) -> List[List[str]]:
+        options = [o for o in set(dictionary) - set(path)
+                   if distance(o, current) == 1]
+        if not options:
+            return [[]]
+        elif end in options:
+            return [[*path, current, end]]
+        else:
+            result = []
+            for o in options:
+                result += dfs(o, [*path, current])
+            return result
+    result = [r for r in dfs(start, []) if len(r) != 0]
+    if len(result) == 0:
+        return None
+    else:
+        return sorted(result, key=lambda l: len(l))[0]
+
+
+assert solve("dog", "cat", d0) == ["dog", "dot", "dat", "cat"]
+assert solve("dog", "cat", d1) is None
