d85f287c54e45c31b3c25aceb021372de8ddefec
Moye Odiase <moye.odiase@moixa.com>
2023-11-06 20:26:19 +0000

    Added ipython as a tool

--- shell.nix
+++ shell.nix
@@ -1,7 +1,7 @@
 { pkgs ? import <nixpkgs> {} }:
 with pkgs;
 let 
-  pythonPackages = ps: with ps; [ numpy pytest autopep8 ];
+  pythonPackages = ps: with ps; [ numpy pytest autopep8 ipython ];
   py = python311.withPackages pythonPackages;
 in
   mkShell {
