72a61ad64f2d54bf7a6fd0d91ecb45278ed195e4
Moye Odiase <moye.odiase@moixa.com>
2024-02-03 19:32:16 +0000

    leetcode: completed h-index

---
+++ leetcode/h-index.cpp
@@ -0,0 +1,22 @@
+#include <cstdint>
+#include <vector>
+
+using Int = std::uint32_t;
+template <typename T> using Vector = std::vector<T>;
+
+
+Int hIndex(Vector<Int>& citations) {
+	const Int N = citations.size();
+    auto citationCount = Vector<Int>(N+1, 0);
+    for (auto c : citations){
+        if (c >= N) citationCount[N]++;
+        else citationCount[c]++;
+    }
+
+    Int cumulativeSum = 0;
+    for (Int i = N; i >= 0; i--) {
+        cumulativeSum += citationCount[i];
+        if (cumulativeSum >= i) return i;
+    }
+    return cumulativeSum;
+}
