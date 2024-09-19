5cad7996c9b73d2d73345ed57c30057c554e4ae9
Moye <moye@fuseenergy.com>
2024-09-19 17:31:47 +0100

    Update fish_prompt
    Outer square brackets are automatically added due to updates made to my
    config.fish. Removing the existing ones to prevent duplication.

--- shell.nix
+++ shell.nix
@@ -3,13 +3,15 @@ with pkgs;
 let 
   PROJECT_ROOT = builtins.getEnv "PWD";
   asciichartpy = import "${PROJECT_ROOT}/nix/asciichartpy.nix";
-  pythonPackages = ps: with ps; [ numpy pytest autopep8 ipython asciichartpy tabulate flake8 ];
+  pythonPackages = ps: with ps; [ 
+    more-itertools numpy pytest autopep8 ipython asciichartpy tabulate flake8 
+  ];
   py = python311.withPackages pythonPackages;
 in
   mkShell {
     buildInputs = [ py ];
     shellHook = ''
-    export fish_prompt_prefix='[problems]'
+    export fish_prompt_prefix='problems'
     exec fish
     '';
   }
