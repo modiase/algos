09300122444c4a258931b4a92353c3b7ba1c708b
Moye <moye@fuseenergy.com>
2024-09-19 17:32:34 +0100

    Format bubblesort.py

--- sorting/bubblesort.py
+++ sorting/bubblesort.py
@@ -1,15 +1,16 @@
 from typing import Iterable
 
+
 def bubblesort(iterable: Iterable[int]) -> tuple[list[int], int]:
     result = list(iterable)
     swaps = 0
     N = len(result)
     for start in range(1, N):
         for i in range(start, 0, -1):
-            if result[i] < result[i-1]:
+            if result[i] < result[i - 1]:
                 swaps += 1
-                tmp = result[i-1]
-                result[i-1] = result[i]
+                tmp = result[i - 1]
+                result[i - 1] = result[i]
                 result[i] = tmp
                 continue
             break
@@ -17,7 +18,5 @@ def bubblesort(iterable: Iterable[int]) -> tuple[list[int], int]:
     return result, swaps
 
 
-
-
-if __name__ == '__main__':
-    print(bubblesort([5,2,4,1,3]))
+if __name__ == "__main__":
+    print(bubblesort([5, 2, 4, 1, 3]))
