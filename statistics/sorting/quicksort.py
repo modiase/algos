7adcdb6f5f87478e8fa2757a1525f8dc22fa6b4a
Moye <moyeodiase@outlook.com>
2024-10-13 19:19:53 +0100

    Make quicksort callable with just input sequence

--- statistics/sorting/quicksort.py
+++ statistics/sorting/quicksort.py
@@ -4,15 +4,19 @@ import random as rn
 from statistics.partition import partition
 
 
-def quicksort(A: MutableSequence, p: int, l: int, r: int) -> None:
+def _quicksort(A: MutableSequence, p: int, l: int, r: int) -> None:
     if l == r:
         return
 
     q = partition(A, p, l, r)
     r_left = max(l, q - 1)
-    quicksort(A, r_left, l, r_left)
+    _quicksort(A, r_left, l, r_left)
     l_right = min(q + 1, r)
-    quicksort(A, r, l_right, r)
+    _quicksort(A, r, l_right, r)
+
+
+def quicksort(A):
+    return _quicksort(A, (N := len(A)) - 1, 0, N - 1)
 
 
 def test_quicksort():
@@ -21,6 +25,6 @@ def test_quicksort():
     for _ in range(I):
         A = [rn.randint(0, 100) for _ in range(N)]
         A_cpy = A[:]
-        quicksort(A_cpy, N - 1, 0, N - 1)
+        quicksort(A_cpy)
         sorted_A = sorted(A)
         assert A_cpy == sorted_A, f"{A_cpy}, {sorted_A}"
