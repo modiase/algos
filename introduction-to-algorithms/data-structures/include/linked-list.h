ea7ba69840777786020430452e61c6683e52a184
Moye <moye@fuseenergy.com>
2024-11-28 22:48:17 +0000

    Add initial linked list implementation

---
+++ introduction-to-algorithms/data-structures/include/linked-list.h
@@ -0,0 +1,12 @@
+#include <stddef.h>
+
+#ifndef __LINKED_LIST_H
+#define __LINKED_LIST_H
+	struct Node {
+		void* data;
+		struct Node *next;
+	};
+	const static struct Node NIL = (struct Node) { .data = NULL, .next = NULL };
+
+	struct Node* LinkedList__prepend(const struct Node *head, struct Node *item);
+#endif
---
+++ introduction-to-algorithms/data-structures/linked-list.c
@@ -0,0 +1,7 @@
+#include "linked-list.h"
+
+
+struct Node* LinkedList__prepend(const struct Node *head, struct Node *item){
+	(*item).next = head;
+	return item;
+}
