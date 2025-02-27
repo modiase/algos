8da757d8b312b32ba861f2ba7bfaab16a62a6845
Moye <moyeodiase@outlook.com>
2025-02-27 10:34:40 +0000

    Update shell.nix
     - Remove exec fish to allow more flexibility in shell used.
     - Add graphviz and sortedcollections packages.

--- shell.nix
+++ shell.nix
@@ -4,14 +4,12 @@ let
   PROJECT_ROOT = builtins.getEnv "PWD";
   asciichartpy = import "${PROJECT_ROOT}/nix/asciichartpy.nix";
   pythonPackages = ps: with ps; [ 
-    more-itertools numpy pytest autopep8 ipython asciichartpy tabulate flake8 
+    more-itertools numpy pytest autopep8 ipython asciichartpy tabulate flake8  graphviz sortedcollections
   ];
   py = python311.withPackages pythonPackages;
 in
   mkShell {
     buildInputs = [ py ];
     shellHook = ''
-    export fish_prompt_prefix='problems'
-    exec fish
     '';
   }
