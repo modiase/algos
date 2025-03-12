d4401428a18aaa777fc177b9de6a1a9a87d20263
Moye <moyeodiase@outlook.com>
2025-03-12 18:37:38 +0000

    Add solution for 1-0 knapsack

---
+++ introduction-to-algorithms/16-greedy-algorithms/1-0-knapsack.py
@@ -0,0 +1,129 @@
+from collections.abc import Collection
+from dataclasses import dataclass
+from itertools import chain, combinations
+from typing import NamedTuple
+
+
+@dataclass(frozen=True, kw_only=True)
+class Item:
+    value: int
+    weight: int
+
+
+def knapsack(items: Collection[Item], W: int) -> int:
+    N = len(items)
+    if N == 0 or W == 0:
+        return 0
+    dp = [[0] * (W + 1) for _ in range(N + 1)]
+
+    for j in range(W + 1):
+        for i in range(1, N + 1):
+            if j < items[i - 1].weight:
+                dp[i][j] = dp[i - 1][j]
+                continue
+            dp[i][j] = max(
+                dp[i - 1][j], dp[i - 1][j - items[i - 1].weight] + items[i - 1].value
+            )
+    return dp[N][W]
+
+
+def brute_force_knapsack(items: Collection[Item], W: int) -> Collection[Item]:
+    return max(
+        filter(
+            lambda c: sum(item.weight for item in c) <= W,
+            chain.from_iterable(combinations(items, i) for i in range(len(items) + 1)),
+        ),
+        key=lambda x: sum(item.value for item in x),
+    )
+
+
+class TestCase(NamedTuple):
+    description: str
+    items: Collection[Item]
+    W: int
+
+
+test_cases = [
+    TestCase(
+        description="Three items, capacity 5",
+        items=[
+            Item(value=10, weight=2),
+            Item(value=20, weight=3),
+            Item(value=30, weight=4),
+        ],
+        W=5,
+    ),
+    TestCase(description="Empty items", items=[], W=10),
+    TestCase(
+        description="Two items, capacity 0",
+        items=[Item(value=10, weight=2), Item(value=20, weight=3)],
+        W=0,
+    ),
+    TestCase(
+        description="Three items with same value, capacity 3",
+        items=[
+            Item(value=10, weight=1),
+            Item(value=10, weight=2),
+            Item(value=10, weight=3),
+        ],
+        W=3,
+    ),
+    TestCase(
+        description="Three items with different values, capacity 4",
+        items=[
+            Item(value=5, weight=2),
+            Item(value=10, weight=2),
+            Item(value=15, weight=2),
+        ],
+        W=4,
+    ),
+    TestCase(
+        description="Ten items with value equals weight, capacity 15",
+        items=[Item(value=i, weight=i) for i in range(1, 11)],
+        W=15,
+    ),
+    TestCase(
+        description="Three items, capacity 4",
+        items=[
+            Item(value=10, weight=5),
+            Item(value=20, weight=6),
+            Item(value=30, weight=7),
+        ],
+        W=4,
+    ),
+    TestCase(
+        description="Three items, capacity 5",
+        items=[
+            Item(value=10, weight=2),
+            Item(value=20, weight=3),
+            Item(value=30, weight=5),
+        ],
+        W=5,
+    ),
+    TestCase(
+        description="Three items, capacity 10",
+        items=[
+            Item(value=100, weight=10),
+            Item(value=10, weight=1),
+            Item(value=20, weight=2),
+        ],
+        W=10,
+    ),
+    TestCase(
+        description="Three items with two items of weight 0, capacity 5",
+        items=[
+            Item(value=10, weight=0),
+            Item(value=20, weight=0),
+            Item(value=30, weight=5),
+        ],
+        W=5,
+    ),
+]
+if __name__ == "__main__":
+    for idx, case in enumerate(test_cases):
+        bf = brute_force_knapsack(case.items, case.W)
+        expected = sum(item.value for item in bf)
+        computed = knapsack(case.items, case.W)
+        assert computed == expected, (
+            f"{idx=}: {case.description}\n{case.items=}\n{case.W=}\n{expected=}\n{computed=}\n{bf=}\n"
+        )
